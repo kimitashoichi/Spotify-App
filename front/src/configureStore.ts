bc094fdd435a2c8298305c39921e155fcefbbe9a
syoichi kimita <kimi.syou.koma@gmail.com>
2021-01-09 16:26:25 +0900

    Fixed so that you can see it even if the extension is not installed

--- front/src/configureStore.ts
+++ front/src/configureStore.ts
@@ -1,23 +1,16 @@
 import { applyMiddleware, createStore } from "redux";
 import { History } from "history";
 import createSagaMiddleware from "redux-saga";
-import { compose } from '@material-ui/system';
 
 import rootSaga from "./sagas";
 import rootReducer from "./reducers";
-import { ExtendedWindow } from "./reduxDevToolExtensionType";
-
-declare var window: ExtendedWindow;
 
 export default function configureStore (history: History) {
   const sagaMiddleware = createSagaMiddleware();
-  // TODO: オペレーター && と || を混ぜて使うなと言う警告がコンソールに出ているが、開発環境でのみ使用するので一旦無視でOK
-  const composeReduxDevToolExtension = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const enhanser = composeReduxDevToolExtension(applyMiddleware(sagaMiddleware));
 
   const store = createStore(
     rootReducer(history),
-    enhanser
+    applyMiddleware(sagaMiddleware)
   );
 
   sagaMiddleware.run(rootSaga);
--- front/src/reduxDevToolExtensionType.ts
+++
@@ -1,6 +0,0 @@
-import { compose } from "redux";
-
-//  redux-devtool-extension type
-export interface ExtendedWindow extends Window {
-  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-}
